91cd9008969d44cf1de442c2b5e6a8c959c7e155
Samu Correa <124720302+samucorre@users.noreply.github.com>
2024-10-06 23:07:04 +0200

    background?

--- app/src/main/java/pf/dam/MainActivity.kt
+++ app/src/main/java/pf/dam/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setBackgroundDrawableResource(R.drawable.background)
         setContentView(R.layout.activity_main)
 
         // Inicializar dbHelpers
