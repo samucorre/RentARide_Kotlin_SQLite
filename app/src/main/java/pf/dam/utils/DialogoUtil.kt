9dc8a03f03438f4f8a64e220af82004f0ce554c6
Samu Correa <124720302+samucorre@users.noreply.github.com>
2024-11-04 22:30:11 +0100

    estilos dialogo

--- app/src/main/java/pf/dam/utils/DialogoUtil.kt
+++ app/src/main/java/pf/dam/utils/DialogoUtil.kt
@@ -1,10 +1,16 @@
 package pf.dam.utils
 
 import android.content.Context
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 class DialogoUtil( context: Context) {
 
@@ -49,7 +55,14 @@ fun ShowDeleteConfirmationDialog(
         title = { Text(text = title) },
         text = { Text(text = message) },
         confirmButton = {
-            TextButton(onClick = onPositiveButtonClick) {
+            TextButton(
+                onClick = onPositiveButtonClick,
+                modifier = Modifier,
+                colors = ButtonDefaults.textButtonColors(
+                    containerColor = Color.Red, // Color de fondo rojo
+                    contentColor = MaterialTheme.colorScheme.onError  // Color del texto en contraste
+                )
+            ) {
                 Text(text = positiveButtonText)
             }
         },
@@ -57,6 +70,8 @@ fun ShowDeleteConfirmationDialog(
             TextButton(onClick = onDismissRequest) {
                 Text(text = negativeButtonText)
             }
-        }
+        },
+        shape = RoundedCornerShape(8.dp), // Forma redondeada (opcional)
+        containerColor =  Color(0xFFFFF2CC)// Color de fondo del diálogo
     )
 }
